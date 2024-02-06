19e2d172fa02fd5e1dfdae36e5f8a91b7af04ec4
Ryan Hamilton <ryangilberthamilton@gmail.com>
2024-02-06 13:50:33 -0500

    Refactor time series tools to include additional functionality

--- src/time_series_tools.py
+++ src/time_series_tools.py
@@ -25,7 +25,7 @@ class HarmonicTimeSeries:
     
     @property
     def frequencies(self):
-        return [mode for mode in self.modes]
+        return [mode for mode in range(1, self.modes + 1)]
     
     def add_constant(self) -> HarmonicTimeSeries:
         self.dataset = self.dataset.map(lambda image: image.addBands(ee.Image(1)))
@@ -34,28 +34,30 @@ class HarmonicTimeSeries:
     
     def add_time(self) -> HarmonicTimeSeries:
         def _add_time(image):
-            year = ee.Image(image).date().difference(ee.Date('1970-01-01'), 'year')
-            time_radiants = year.multiply(2 * pi)
-            return image.addBands(time_radiants.rename('t').float())
+            date = date = ee.Date(image.get('system:time_start'))
+            year = date.difference(ee.Date('1970-01-01'), 'year')
+            time_radians = ee.Image(year.multiply(2 * pi * 1))
+            return image.addBands(time_radians.rename('t').float())
         self.dataset = self.dataset.map(_add_time)
         self._independnet.append('t')
         return self
     
     def add_harmonics(self) -> HarmonicTimeSeries:
-        cosines = self._get_names('cos', self.modes)
-        sines = self._get_names('sin', self.modes)
+        cosine = self._get_names('cos', self.modes)
+        sine = self._get_names('sin', self.modes)
         def _add_harmoncis(image):
             freq = ee.Image.constant(self.frequencies)
             time = image.select('t')
-            cosines = time.multiply(freq).cos().rename(cosines)
-            sines = time.multiply(freq).sin().rename(sines)
+            cosines = time.multiply(freq).cos().rename(cosine)
+            sines = time.multiply(freq).sin().rename(sine)
             return image.addBands(cosines).addBands(sines)
         self.dataset = self.dataset.map(_add_harmoncis)
-        self._independnet.extend(cosines + sines)
+        self._independnet.extend(cosine + sine)
+        return self
             
     def compute_trend(self) -> HarmonicTimeSeries:
         self.trend = (
-            self.dataset.select(self.dependent + self._independnet)
+            self.dataset.select([self.dependent_variable] + self._independnet)
             .reduce(ee.Reducer.linearRegression(len(self._independnet), 1))    
         )
         return self
@@ -63,7 +65,6 @@ class HarmonicTimeSeries:
     def compute_coefficients(self) -> HarmonicTimeSeries:
         self.coefficients = (
             self.trend.select('coefficients')
-            .arrayProject([0])
             .arrayFlatten([self._independnet, ['coef']])
         )
         self.dataset = self.dataset.map(lambda image: image.addBands(self.coefficients))
@@ -81,7 +82,7 @@ class HarmonicTimeSeries:
 
     @staticmethod
     def _get_names(prefix, modes):
-        return [f'{prefix}_{mode}' for mode in modes]
+        return [f'{prefix}_{mode}' for mode in range(1, modes + 1)]
 
 
 class FourierTransform(HarmonicTimeSeries):
@@ -112,8 +113,8 @@ class FourierTransform(HarmonicTimeSeries):
     def process(self) -> ee.Image:
         dataset = super().process()
         
-        for mode in self.modes:
+        for mode in range(1, self.modes + 1):
             dataset = dataset.compute_phase(mode)
             dataset = dataset.compute_amplitude(mode)
-        
-        return self.tranform()
+        pattern = f"{self.dependent_variable}|.*coef|amp.*|phase.*"
+        return self.tranform().select(pattern)
