e6cdf13bc9d33865c1238a76d91b01053e91a0ff
Ryan Hamilton <ryangilberthamilton@gmail.com>
2024-02-06 13:52:46 -0500

    Add install_requires to setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ setup(
     name='time_series_tools',
     version='0.1.0',
     package_dir={'': 'src'},
-    requires=['earthengine-api'],
     author='Ryan Hamilton',
-    author_email='ryan.hamilton@ec.gc.ca'
+    author_email='ryan.hamilton@ec.gc.ca',
+    install_requires=[
+        'earthengine-api',
+    ]
 )
